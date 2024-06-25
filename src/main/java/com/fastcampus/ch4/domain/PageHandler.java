46941d546b3a07687d98afafef36f6ef6922f733
seohan1010 <seohan1010@hotmail.com>
2024-06-25 10:51:31 +0900

    Update PageHandler.java comment  deleted

--- src/main/java/com/fastcampus/ch4/domain/PageHandler.java
+++ src/main/java/com/fastcampus/ch4/domain/PageHandler.java
@@ -1,15 +1,15 @@
 package com.fastcampus.ch4.domain;
 
 public class PageHandler {
-    private int totalCnt; // 총 게시물 개수
-    private int pageSize; // 한 페이지의 크기
-    private int naviSize = 10; // 페이지 네비게이션의 크기
-    private int totalPage; // 전체 페이지의 개수
-    private int page; // 현재 페이지
-    private int beginPage; // 네비게이션의 첫번째 페이지
-    private int endPage; // 네비게이션의 마지막 페이지
-    private boolean showPrev; // 이전 페이지로 이동하는 링크를 보여줄 것인지의 여부
-    private boolean showNext; // 다음 페이지로 이동하는 링크를 보여줄 것인지의 여부
+    private int totalCnt; 
+    private int pageSize; 
+    private int naviSize = 10; 
+    private int totalPage; 
+    private int page; 
+    private int beginPage;
+    private int endPage; 
+    private boolean showPrev; 
+    private boolean showNext; 
 
     public PageHandler(int totalCnt, int page) {
         this(totalCnt, page, 10);
@@ -21,12 +21,12 @@ public class PageHandler {
         this.page = page;
 
         totalPage = (int) Math.ceil(totalCnt / (double)pageSize);
-        // 일의 자리를 없애기 위해서 10으로 나누고 1을 더해준다.
+      
         beginPage = (page-1) / naviSize * naviSize + 1;
         endPage = Math.min(beginPage-1 + naviSize, totalPage);
-        // 시작하는 페이지가 1이면은 앞으로 이동하는 화살표를 보여주지 않는다.
+        
         showPrev = beginPage != 1;
-        //
+      
         showNext = endPage != totalPage;
 
 
