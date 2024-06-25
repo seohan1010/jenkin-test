8cfe32098a146c1a568f9b9e1ebbcfc504df128d
seohan1010 <seohan1010@hotmail.com>
2024-06-25 10:56:41 +0900

    Update BoardController.java comment modified

--- src/main/java/com/fastcampus/ch4/controller/BoardController.java
+++ src/main/java/com/fastcampus/ch4/controller/BoardController.java
@@ -56,9 +56,9 @@ public class BoardController {
     }
 
     private boolean loginCheck(HttpServletRequest request) {
-        // 1. 세션을 얻어서
+    
         HttpSession session = request.getSession();
-        // 2. 세션에 id가 있는지 확인, 있으면 true를 반환
+ 
         return session.getAttribute("id") != null;
     }
 }
